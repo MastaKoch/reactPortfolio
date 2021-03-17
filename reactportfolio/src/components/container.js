f80a28f6ef5a20bcf6db78d6efea88314cea9122
MastaKoch <alexkoch1996@gmail.com>
2021-03-17 15:16:19 -0400

    initial commit with some files to alter

---
+++ reactportfolio/src/components/container.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Container from 'react-bootstrap/Container';
+
+
+class AppContainer extends Component {
+
+    render() {
+        return (
+            <Container fluid>
+              <Row>
+                <Col>1 of 1</Col>
+              </Row>
+            </Container>
+                )
+        
+
+    }
+    
+  }
+
+  export default AppContainer;
--- reactportfolio/src/index.js
+++ reactportfolio/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/navbar';
+import Navbar from 'react-bootstrap/esm/Navbar';
+import Container from './components/container';
+import container from 'react-bootstrap/Container';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContainer>
+      
+
+    </AppContainer>
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
