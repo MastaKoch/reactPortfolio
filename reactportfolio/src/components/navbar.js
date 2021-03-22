031d735c859e89fb7481efcd9f7ea708b6698f63
MastaKoch <alexkoch1996@gmail.com>
2021-03-22 14:15:08 -0400

    finished adding icons

--- reactportfolio/src/components/navbar.js
+++ reactportfolio/src/components/navbar.js
@@ -5,6 +5,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Button from 'react-bootstrap/Button';
+import { SocialIcon } from 'react-social-icons';
 
 const renderTooltip = (props) => (
   <Tooltip id="button-tooltip" {...props}>
@@ -28,18 +29,19 @@ class Navigate extends Component {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <NavDropdown title="For Employers" id="basic-nav-dropdown">
-              <NavDropdown.Item href="https://www.linkedin.com/in/alex-koch-309641184/"target="_blank">LinkedIn</NavDropdown.Item>
-              <NavDropdown.Item href="https://github.com/MastaKoch"target="_blank">GitHub</NavDropdown.Item>
+              <NavDropdown.Item href="https://www.linkedin.com/in/alex-koch-309641184/"target="_blank"><SocialIcon network="linkedin" style={{ height: 25, width: 25, margin: 10 }} />LinkedIn</NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/MastaKoch"target="_blank"><SocialIcon network="github" style={{ height: 25, width: 25, margin: 10 }} />GitHub</NavDropdown.Item>
               <NavDropdown.Divider />
+              <NavDropdown.Item href="https://drive.google.com/file/d/117qVyWiVrD34et0vP4NBfhnZFcm1yExy/view?usp=sharing" target= "_blank"><SocialIcon network="google" style={{ height: 25, width: 25, margin: 10 }} />Resume</NavDropdown.Item>
               <NavDropdown.Item>
                 <OverlayTrigger
                     placement="right"
                     delay={{ show: 250, hide: 400 }}
                     overlay={renderTooltip}
                   >
-                <Button variant="light">Email</Button>
+                <Button variant="light"><SocialIcon network="email" style={{ height: 25, width: 25, margin: 10 }} />Email</Button>
   </OverlayTrigger></NavDropdown.Item>
-              <NavDropdown.Item href="https://drive.google.com/file/d/117qVyWiVrD34et0vP4NBfhnZFcm1yExy/view?usp=sharing" target= "_blank">Resume</NavDropdown.Item>
+              
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
