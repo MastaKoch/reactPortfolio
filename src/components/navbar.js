dcab140328741c0f861e94a14e1aa5dc713b228b
MastaKoch <alexkoch1996@gmail.com>
2021-03-23 10:31:58 -0400

    fixed email dropdown

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,16 +2,16 @@ import React, {Component} from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Tooltip from 'react-bootstrap/Tooltip';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Button from 'react-bootstrap/Button';
+// import Tooltip from 'react-bootstrap/Tooltip';
+// import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+// import Button from 'react-bootstrap/Button';
 import { SocialIcon } from 'react-social-icons';
 
-const renderTooltip = (props) => (
-  <Tooltip id="button-tooltip" {...props}>
-    alexkoch1996@gmail.com
-  </Tooltip>
-);
+// const renderTooltip = (props) => (
+//   <Tooltip id="button-tooltip" {...props}>
+//     alexkoch1996@gmail.com
+//   </Tooltip>
+// );
 
 
   
@@ -34,13 +34,10 @@ class Navigate extends Component {
               <NavDropdown.Divider />
               <NavDropdown.Item href="https://drive.google.com/file/d/117qVyWiVrD34et0vP4NBfhnZFcm1yExy/view?usp=sharing" target= "_blank"><SocialIcon network="google" style={{ height: 25, width: 25, margin: 10 }} />Resume</NavDropdown.Item>
               <NavDropdown.Item>
-                <OverlayTrigger
-                    placement="right"
-                    delay={{ show: 250, hide: 400 }}
-                    overlay={renderTooltip}
-                  >
-                <Button variant="light"><SocialIcon network="email" style={{ height: 25, width: 25, margin: 10 }} />Email</Button>
-  </OverlayTrigger></NavDropdown.Item>
+             <SocialIcon network="email" style={{ height: 25, width: 25, margin: 10 }} /><a href="/" title="alexkoch1996@gmail.com" className="aTag">Email</a>
+
+
+              </NavDropdown.Item>
               
             </NavDropdown>
           </Nav>
